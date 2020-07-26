f5461c07c9c1637dff064151786be160baf2459c
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-26 22:42:56 +0545

    loading style using flutter_spinkit

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -27,9 +28,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Loading...'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitThreeBounce(
+          color: Colors.white,
+          size: 50.0,
+        ),
       ),
     );
   }
